1666a3158e7b0d98825a7250d94b6d1e62cf4600
Maggie Merrell <maggiemerrell31@gmail.com>
2016-06-28 11:31:14 -0400

    backbone of guess who

---
+++ main.js
@@ -0,0 +1,7 @@
+gameChoice = prompt("Would you like to play Guess Who or Memory?");
+  if (gameChoice === "Guess Who") {
+    window.location.assign(index.html);
+  }
+
+//prompt of which game to pick
+//array of artists, paintings and attributes
