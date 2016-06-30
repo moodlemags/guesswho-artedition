5cff0c4d0184d14181dc69cc733db6b9c6e0795d
Maggie Merrell <maggiemerrell31@gmail.com>
2016-06-30 11:17:09 -0400

    added more artists and attributes

--- main.js
+++ main.js
@@ -8,9 +8,7 @@
 
 
 //MODEL - ARRAY OF PAINTINGS THAT BECOME THE RANDOM CARD
-var paintingsArray = ["images/paintings/avedon-boyd.png", "images/paintings/bacon-self-portrait.jpg",  "images/paintings/basquiat-trumpet.jpg", "images/paintings/bochner-syntax.jpg", "images/paintings/bonnard-decor.jpg"]
-
-// "images/paintings/caravaggio-boy-lizard.jpg", "images/paintings/cassatt-bath.jpg", "images/paintings/chagall-paysage.jpg", "images/paintings/davinci-ermine.jpg", "images/paintings/dali-elephants.jpg", "images/paintings/defeo-rose.jpg", "images/paintings/degas-danceclass.png", "images/paintings/johns-racing.jpg", "images/paintings/kahlo-roots.jpg",  "images/paintings/klimt-kiss.jpg", "images/paintings/koons-doh.jpg", "images/paintings/kruger-battleground.jpg"]
+var paintingsArray = ["images/paintings/avedon-boyd.png",  "images/paintings/basquiat-trumpet.jpg", "images/paintings/bochner-syntax.jpg", "images/paintings/bonnard-decor.jpg", "images/paintings/caravaggio-boy-lizard.jpg", "images/paintings/cassatt-bath.jpg", "images/paintings/chagall-paysage.jpg", "images/paintings/bacon-self-portrait.jpg", "images/paintings/creed-ilikethings.jpg", "images/paintings/davinci-ermine.jpg", "images/paintings/defeo-rose.jpg", "images/paintings/degas-danceclass.png", "images/paintings/johns-racing.jpg", "images/paintings/kahlo-roots.jpg", "images/paintings/klimt-kiss.jpg", "images/paintings/kruger-battleground.jpg", "images/paintings/magritte-golconda.jpg", "images/paintings/modigliani-portrait.jpg"]
 
 
 //MODEL -- ARTISTS ARRAY w/ DATA
@@ -27,18 +25,7 @@ var artists = [
       artwork: paintingsArray[0],
       html: document.getElementsByClassName('avedon')
     },
-    { name: "Francis Bacon",
-      isAlive: false,
-      gender: "male",
-      nationality: "british",
-      facialHair: false,
-      hair: "average",
-      decade: "20th century",
-      bearded: false,
-      stillValid: true,
-      artwork: paintingsArray[1],
-      html: document.getElementsByClassName('bacon')
-    },
+
     { name: "Jean-Michel Basquiat ",
       isAlive: false,
       gender: "male",
@@ -48,7 +35,7 @@ var artists = [
       decade: "20th century",
       bearded: false,
       stillValid: true,
-      artwork: paintingsArray[2],
+      artwork: paintingsArray[1],
       html: document.getElementsByClassName('basquiat')
     },
     { name: "Mel Bochner",
@@ -60,7 +47,7 @@ var artists = [
       decade: "20th century",
       bearded: false,
       stillValid: true,
-      artwork: paintingsArray[3],
+      artwork: paintingsArray[2],
       html: document.getElementsByClassName('bochner')
     },
     { name: "Pierre Bonnard",
@@ -72,8 +59,176 @@ var artists = [
       decade: "19th century",
       bearded: false,
       stillValid: true,
-      artwork: paintingsArray[4],
+      artwork: paintingsArray[3],
       html: document.getElementsByClassName('bonnard')
+    },
+    { name: "Michelangelo Merisi da Caravaggio",
+      isAlive: false,
+      gender: "male",
+      nationality: "italian",
+      facialHair: true,
+      hair: "cool hair",
+      decade: "16th century",
+      bearded: false,
+      stillValid: true,
+      artwork: paintingsArray[4],
+      html: document.getElementsByClassName('caravaggio')
+    },
+    { name: "Mary Cassatt",
+      isAlive: false,
+      gender: "female",
+      nationality: "american",
+      facialHair: false,
+      hair: "average",
+      decade: "19th century",
+      bearded: false,
+      stillValid: true,
+      artwork: paintingsArray[5],
+      html: document.getElementsByClassName('cassatt')
+    },
+    { name: "Marc Chagall",
+      isAlive: false,
+      gender: "male",
+      nationality: "french",
+      facialHair: false,
+      hair: "average",
+      decade: "20th century",
+      bearded: false,
+      stillValid: true,
+      artwork: paintingsArray[6],
+      html: document.getElementsByClassName('chagall')
+    },
+    { name: "Martin Creed",
+      isAlive: true,
+      gender: "male",
+      nationality: "british",
+      facialHair: false,
+      hair: "average",
+      decade: "20th century",
+      bearded: false,
+      stillValid: true,
+      artwork: paintingsArray[7],
+      html: document.getElementsByClassName('creed')
+    },
+    { name: "Salvador Dali",
+      isAlive: false,
+      gender: "male",
+      nationality: "spanish",
+      facialHair: true,
+      hair: "average",
+      decade: "20th century",
+      bearded: false,
+      stillValid: true,
+      artwork: paintingsArray[8],
+      html: document.getElementsByClassName('dali')
+    },
+    { name: "Leonardo da Vinci",
+      isAlive: false,
+      gender: "male",
+      nationality: "italian",
+      facialHair: true,
+      hair: "cool hair",
+      decade: "15th century",
+      bearded: true,
+      stillValid: true,
+      artwork: paintingsArray[9],
+      html: document.getElementsByClassName('da-vinci')
+    },
+    { name: "Jay DeFeo",
+      isAlive: false,
+      gender: "female",
+      nationality: "american",
+      facialHair: false,
+      hair: "cool hair",
+      decade: "20th century",
+      bearded: false,
+      stillValid: true,
+      artwork: paintingsArray[10],
+      html: document.getElementsByClassName('defeo')
+    },
+    { name: "Edgar Degas",
+      isAlive: false,
+      gender: "male",
+      nationality: "french",
+      facialHair: true,
+      hair: "average",
+      decade: "19th century",
+      bearded: true,
+      stillValid: true,
+      artwork: paintingsArray[11],
+      html: document.getElementsByClassName('degas')
+    },
+    { name: "Jasper Johns",
+      isAlive: true,
+      gender: "male",
+      nationality: "american",
+      facialHair: false,
+      hair: "average",
+      decade: "20th century",
+      bearded: false,
+      stillValid: true,
+      artwork: paintingsArray[12],
+      html: document.getElementsByClassName('johns')
+    },
+    { name: "Frieda Kahlo",
+      isAlive: false,
+      gender: "female",
+      nationality: "mexican",
+      facialHair: false,
+      hair: "cool hair",
+      decade: "20th century",
+      bearded: false,
+      stillValid: true,
+      artwork: paintingsArray[13],
+      html: document.getElementsByClassName('kahlo')
+    },
+    { name: "Gustav Klimt",
+      isAlive: false,
+      gender: "male",
+      nationality: "austrian",
+      facialHair: true,
+      hair: "cool hair",
+      decade: "19th century",
+      bearded: true,
+      stillValid: true,
+      artwork: paintingsArray[14],
+      html: document.getElementsByClassName('klimt')
+    },
+    { name: "Barbara Kruger",
+      isAlive: true,
+      gender: "female",
+      nationality: "american",
+      facialHair: false,
+      hair: "cool hair",
+      decade: "21st century",
+      bearded: false,
+      stillValid: true,
+      artwork: paintingsArray[15],
+      html: document.getElementsByClassName('kruger')
+    },
+    { name: "Rene Magritte",
+      isAlive: false,
+      gender: "male",
+      nationality: "french",
+      facialHair: false,
+      hair: "average",
+      decade: "20th century",
+      bearded: false,
+      stillValid: true,
+      artwork: paintingsArray[16],
+      html: document.getElementsByClassName('magritte')
+    },
+    { name: "Amedeo Modigliani",
+      isAlive: false,
+      gender: "male",
+      nationality: "italian",
+      facialHair: false,
+      hair: "average",
+      decade: "19th century",
+      bearded: false,
+      stillValid: true,
+      artwork: paintingsArray[17],
+      html: document.getElementsByClassName('modigliani')
     }
   ]
 //COMPLETE - MODEL --- alert with rules of how to play once content has loaded
@@ -100,48 +255,93 @@ document.addEventListener('DOMContentLoaded', function(){
               console.log(mysteryArtist);
             });
 
-      //  mysteryArtist = paintingsArray.indexOf(paintingsArray[mysteryCard]);
-      //         console.log(mysteryArtist);
-              // console.log(artists[mysteryArtist].isAlive);
-
 //GAMEBEGIN FUNCTION - Now we start the game aspect, the attributes the user guesses should be compared to mysteryArtist and evaluated if true or false
         var beginGuessing = document.getElementsByClassName("guessButton")[0];
           beginGuessing.addEventListener('click', function(){
               var userInput = (prompt("Is my artist:  "));
               console.log(userInput);
+              console.log(artists);
+
 //large if statement function begins:
               if (userInput == 'alive' || userInput == 'male'|| userInput == 'female' || userInput == 'dead' || userInput == 'moustache' || userInput == 'cool hair' || userInput == '19th century' || userInput == '20th century' || userInput == '15th century' || userInput == ' 16th century' || userInput == 'beard' || userInput == 'american') {
                     if (userInput == 'alive' && artists[mysteryArtist].isAlive == true) {
-                                var node = document.getElementById('displayResults');
+/*gray dead*/                   var node = document.getElementById('displayResults');
                                 var guess = document.createTextNode('you got a live one! all dead artists will be removed from the board');
                                     node.appendChild(guess);
                                 // console.log(node);
                                 // console.log(guess);
                                 for (i=0; i < artists.length; i++) {
-                                  if (artists.isAlive !== true) {
-                                    artists.stillValid = false;
+                                  if (artists[i].isAlive !== true) {
+                                    artists[i].stillValid = false;
                                     console.log(artists)
-                                  } else {
-                                    console.log('alive');
-                                  }
-                                }
-                                console.log('Great question! Your artist is alive. We will now get rid of all dead artists');
+                                  } else { console.log('alive'); }};
                         } else if (userInput == 'alive' && artists[mysteryArtist].isAlive !== true) {
-                            console.log('Oh no! Your artist is dead. I am sorry for your loss')
+/*gray alive*/                 var node = document.getElementById('displayResults');
+                               var guess = document.createTextNode('Oh no! Your artist is dead. I am sorry for your loss -- all living artists will now disappear.');
+                                    node.appendChild(guess);
+                                        for (i=0; i < artists.length; i++) {
+                                          if (artists[i].isAlive == true) {
+                                              artists[i].stillValid = false;
+                                            console.log(artists)
+                                          } else { console.log('dead'); }};
                         } else if (userInput == 'male' && artists[mysteryArtist].gender == 'male') {
-                            console.log('Great question! Your artist is a man. We will now get rid of all female artists');
+/*gray female*/                var node = document.getElementById('displayResults');
+                               var guess = document.createTextNode('Great guess - your artist is male. Bye bye lady artists!');
+                                      node.appendChild(guess);
+                                          for (i=0; i < artists.length; i++) {
+                                              if (artists[i].gender == 'female') {
+                                                  artists[i].stillValid = false;
+                                                console.log(artists)
+                                              } else { console.log('male'); }};
                         } else if (userInput == 'male' && artists[mysteryArtist].gender !== 'male') {
-                            console.log('Nope! Your artist is not a man');
-                        } else if (userInput == 'female' && artists[mysteryArtist].gender !== 'male') {
-                            console.log('Great question! Your artist is a woman. We will now get rid of all male artists');
+/*gray male*/                  var node = document.getElementById('displayResults');
+                               var guess = document.createTextNode('Nope! Not a male artist.');
+                                        node.appendChild(guess);
+                                            for (i=0; i < artists.length; i++) {
+                                               if (artists[i].gender == 'male') {
+                                                  artists[i].stillValid = false;
+                                                  console.log(artists)
+                                            } else { console.log('female'); }};
+                        } else if (userInput == 'female' && artists[mysteryArtist].gender == 'female') {
+/*gray male*/                  var node = document.getElementById('displayResults');
+                               var guess = document.createTextNode('What luck! Your artist is a woman');
+                                          node.appendChild(guess);
+                                              for (i=0; i < artists.length; i++) {
+                                                 if (artists[i].gender == 'male') {
+                                                    artists[i].stillValid = false;
+                                                    console.log(artists)
+                                              } else { console.log('female'); }};
                         } else if (userInput == 'female' && artists[mysteryArtist].gender == 'male') {
-                            console.log('Nope! Your artist is not a woman');
+/*gray female*/                  var node = document.getElementById('displayResults');
+                                 var guess = document.createTextNode('Unfortunately, your artist is not a woman');
+                                          node.appendChild(guess);
+                                              for (i=0; i < artists.length; i++) {
+                                                  if (artists[i].gender == 'female') {
+                                                      artists[i].stillValid = false;
+                                                      console.log(artists)
+                                                  } else { console.log('male'); }};
                         } else if (userInput == 'dead' && artists[mysteryArtist].isAlive !== true) {
-                            console.log('Oh no! Your artist is dead. I am sorry for your loss. But your guess was right!');
+/*gray alive*/                  var node = document.getElementById('displayResults');
+                                var guess = document.createTextNode('Oh no! Your artist is dead. I am sorry for your loss. But your guess was right!');
+                                            node.appendChild(guess);
+                                              for (i=0; i < artists.length; i++) {
+                                                 if (artists[i].isAlive == true) {
+                                                     artists[i].stillValid = false;
+                                                      console.log(artists)
+                                                } else { console.log('dead'); }};
                         } else if (userInput == 'dead' && artists[mysteryArtist].isAlive == true) {
-                            console.log('Bad luck! Your artist is alive, but your guess was wrong');
+/*WRONG is alive*/            var node = document.getElementById('displayResults');
+                              var guess = document.createTextNode('Bad luck! Your artist is alive, but your guess was wrong');
+                                            node.appendChild(guess);
                         } else if (userInput == 'moustache' && artists[mysteryArtist].facialHair == true) {
-                            console.log('Awesome! Your artist has cool artistic facial hair');
+/*gray n/facialHair*/          var node = document.getElementById('displayResults');
+                               var guess = document.createTextNode('Awesome! Your artist has cool artistic facial hair');
+                                            node.appendChild(guess);
+                                            for (i=0; i < artists.length; i++) {
+                                                if (artists[i].facialHair !== true) {
+                                                  artists[i].stillValid = false;
+                                                  console.log(artists)
+                                            } else { console.log('moustached '); }};
                         } else if (userInput == 'moustache' && artists[mysteryArtist].facialHair !== true) {
                             console.log('Too bad. Your artist does not have cool facial hair');
                         } else if (userInput == 'cool hair' && artists[mysteryArtist].hair == 'cool hair') {
